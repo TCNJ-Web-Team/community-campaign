52a88f8c9e9292e24e2cf4f8e7d76f319f0f5211
lvl12sealclubber <adam.holsten@gmail.com>
2023-10-27 16:25:40 -0400

    not working

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -1,11 +1,28 @@
-import React from "react";
-import { Parallax } from "react-parallax";
+// import React from "react";
+// import { Parallax } from "react-parallax";
 
-const ParallaxContainer = ({ image, alt, strength = 100, children, align }) => (
-  <Parallax bgImage={image} bgImageAlt={alt} strength={strength}>
-    <div className={align}>{children}</div>
-  </Parallax>
-);
+// const ParallaxContainer = ({ image, alt, strength = 100, children, align }) => (
+//   <Parallax bgImage={image} bgImageAlt={alt} strength={strength}>
+//     <div className={align}>{children}</div>
+//   </Parallax>
+// );
 
-// https://www.npmjs.com/package/react-parallax
+// // https://www.npmjs.com/package/react-parallax
+// export default ParallaxContainer;
+
+import { ParallaxBanner } from "react-scroll-parallax";
+
+const ParallaxContainer = () => {
+  return (
+    <ParallaxBanner
+      layers={[
+        {
+          image: "https://react-scroll-parallax.damnthat.tv/img/banner.jpg",
+          speed: -15,
+        },
+      ]}
+      className="aspect-[2/1]"
+    />
+  );
+};
 export default ParallaxContainer;
