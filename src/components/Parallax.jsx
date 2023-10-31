77e42a12b5cae7713ae5ed217e93b01381feba5b
lvl12sealclubber <adam.holsten@gmail.com>
2023-10-31 13:09:47 -0400

    update parallax

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -1,11 +1,25 @@
-import React from "react";
-import { Parallax } from "react-parallax";
+// import React from "react";
+// import { Parallax } from "react-parallax";
 
-const ParallaxContainer = ({ image, alt, strength = 100, children, align }) => (
-  <Parallax bgImage={image} bgImageAlt={alt} strength={strength}>
-    <div className={align}>{children}</div>
-  </Parallax>
-);
+// const ParallaxContainer = ({ image, alt, strength = 100, children, align }) => (
+//   <Parallax bgImage={image} bgImageAlt={alt} strength={strength}>
+//     <div className={align}>{children}</div>
+//   </Parallax>
+// );
 
-// https://www.npmjs.com/package/react-parallax
-export default ParallaxContainer;
+// // https://www.npmjs.com/package/react-parallax
+// export default ParallaxContainer;
+
+import { motion } from "framer-motion";
+
+export default function ParallaxContainer({ children }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      {children}
+    </motion.div>
+  );
+}
