9e495d566f4c65ebfce1abd635c64b8228d0c068
lvl12sealclubber <adam.holsten@gmail.com>
2023-10-31 13:49:55 -0400

    Update Parallax.jsx

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -1,4 +1,3 @@
-// import React from "react";
 // import { Parallax } from "react-parallax";
 
 // const ParallaxContainer = ({ image, alt, strength = 100, children, align }) => (
@@ -10,16 +9,33 @@
 // // https://www.npmjs.com/package/react-parallax
 // export default ParallaxContainer;
 
-import { motion } from "framer-motion";
+import React, { useState, useRef, useEffect } from "react";
+import { motion, useInView, useAnimation } from "framer-motion";
 
 export default function ParallaxContainer({ children }) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+  const mainControls = useAnimation();
+  useEffect(() => {
+    if (isInView) {
+      // console.log("in view");
+      mainControls.start("visible");
+    }
+  }, [isInView]);
+
   return (
-    <motion.div
-      initial={{ opacity: 0, scale: 0.5 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 0.5 }}
-    >
-      {children}
-    </motion.div>
+    <div className="wraper" ref={ref}>
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, scale: 0.5 },
+          visible: { opacity: 1, scale: 1 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 0.5 }}
+      >
+        {children}
+      </motion.div>
+    </div>
   );
 }
