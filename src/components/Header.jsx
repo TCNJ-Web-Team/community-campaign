60be295d22cd73583ff72ae2508aeb7906c33e68
lvl12sealclubber <adam.holsten@gmail.com>
2023-11-08 10:52:18 -0500

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 export default function Header() {
   return (
     <header id="header">
-      <img src="/astro/images/tcnj-logo.png" alt="TCNJ" id="tcnj-logo" />
-      <a href="https://give.tcnj.edu">Support TCNJ</a>
+      <a href="https://.tcnj.edu" id="tcnj-logo">
+        <img src="/astro/images/tcnj-logo.png" alt="TCNJ" />
+      </a>
+      <a href="https://give.tcnj.edu" id="arrow-link">
+        Support TCNJ
+      </a>
     </header>
   );
 }
